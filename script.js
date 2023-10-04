226d8485e26af39e0b2b22253ef5dd5d867e8aa2
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-10-04 15:43:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.id === 'clr') {
+            input.value = '';
+        } else if (button.id === 'ans') {
+            try {
+                input.value = eval(input.value);
+                if (input.value === Infinity) {
+                    input.value = 'Infinity';
+                } else if (isNaN(input.value)) {
+                    input.value = 'NaN';
+                }
+            } catch (error) {
+                input.value = 'Error';
+            }
+        } else {
+            input.value += button.textContent;
+        }
+    });
+});
